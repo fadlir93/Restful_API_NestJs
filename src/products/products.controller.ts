6eab63db6eb8f027d7d523af019021a785f4d92e
Fadli Ramadhan <fadlir93@gmail.com>
2019-11-11 10:45:22 +0700

    sample routes CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get} from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
+
+interface ProductDto {
+    id: string;
+    name: string;
+}
 
 @Controller('products')
 export class ProductsController {
-    constructor(private productService: ProductsService) {}
+    constructor(private productsService: ProductsService) {}
         @Get()
         getProducts() {
-            return this.productService.getProducts();
+            return this.productsService.getProducts();
         }
 
+        @Get(':id')
+        getProduct(@Param() params) {
+            console.log('get a single product', params.id);
+            return this.productsService.getProducts().filter(p => p.id === Number(params.id));
+        }
+
+        @Post()
+        createProduct(@Body() product: ProductDto) {
+            console.log('create product', product);
+            return this.productsService.createProduct(product);
+        }
+
+        @Put()
+        updateProduct(@Body() product: ProductDto) {
+            console.log('update product', product);
+            this.productsService.updateProduct(product);
+        }
+
+        @Delete()
+        deleteProduct(@Body() product: ProductDto) {
+            console.log('delete product', product.id);
+            this.productsService.deleteProduct(product.id);
+        }
 }
